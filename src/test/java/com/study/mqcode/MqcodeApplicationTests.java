c1f6d1ef524290608135392cd6afcf26eb2c442f
LYN_CODE <1304271496@qq.com>
2021-05-05 16:49:36 +0800

    新增原生API异步发送消息

--- src/test/java/com/study/mqcode/MqcodeApplicationTests.java
+++ src/test/java/com/study/mqcode/MqcodeApplicationTests.java
@@ -6,6 +6,7 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
@@ -17,16 +18,13 @@ import java.util.List;
 @SpringBootTest
 class MqcodeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+    //==========下边是同步发送的方式============
     @Test
     public void testRocketMQProduceAPI() throws Exception {
         /**
          * 创建消息发送者 并指定生产者组
          */
-        DefaultMQProducer producer=new DefaultMQProducer("producer-one-test");
+        DefaultMQProducer producer = new DefaultMQProducer("producer-one-test");
         /**
          * 设置nameserver，讲自己交给其管理
          */
@@ -34,13 +32,13 @@ class MqcodeApplicationTests {
         producer.start();
 
         //设置消息主题和消息标签
-        String topic="topic-one";
-        String tag="tag-one";
+        String topic = "topic-one";
+        String tag = "tag-one";
 
         //消息
-        String msg="测试消息";
+        String msg = "测试消息";
 
-        Message message=new Message(topic,tag,msg.getBytes());
+        Message message = new Message(topic, tag, msg.getBytes());
 
         //同步发送
         SendResult send = producer.send(message);
@@ -54,11 +52,11 @@ class MqcodeApplicationTests {
          *
          * 设置消费者组，同一组消费者 必须消费相同的消息
          */
-        DefaultMQPushConsumer consumer=new DefaultMQPushConsumer("consumer-test-one");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer-test-one");
         consumer.setNamesrvAddr("127.0.0.1:9876");
 
         //订阅消息 主题  标签
-        consumer.subscribe("topic-one","tag-one");
+        consumer.subscribe("topic-one", "tag-one");
         //监听消息
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
@@ -70,4 +68,47 @@ class MqcodeApplicationTests {
 
         consumer.start();
     }
+
+    //==========下边是异步发送的方式============
+
+    @Test
+    public void testRocketMQProduceAPIOne() throws Exception {
+        /**
+         * 创建消息发送者 并指定生产者组
+         */
+        DefaultMQProducer producer = new DefaultMQProducer("producer-one-test");
+        /**
+         * 设置nameserver，讲自己交给其管理
+         */
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+
+        producer.setRetryTimesWhenSendAsyncFailed(1000);//设置异步发送失败的超时重试时间
+
+        //设置消息主题和消息标签
+        String topic = "topic-one";
+        String tag = "tag-one";
+
+        //消息
+        String msg = "测试消息";
+
+        Message message = new Message(topic, tag, msg.getBytes());
+
+        //异步发送
+        producer.send(message, new SendCallback() {
+
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.println("消息发送成功的回调函数");
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                System.out.println("消息发送失败的回调函数");
+            }
+        });
+
+    }
+
+
 }
