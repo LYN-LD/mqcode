9f468009bc846120b1db656a9df31ad9b3abfe27
LYN_CODE <1304271496@qq.com>
2021-05-05 17:15:24 +0800

    新增原生API顺序消息

--- src/test/java/com/study/mqcode/MqcodeApplicationTests.java
+++ src/test/java/com/study/mqcode/MqcodeApplicationTests.java
@@ -1,15 +1,15 @@
 package com.study.mqcode;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.consumer.listener.*;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -128,8 +128,6 @@ class MqcodeApplicationTests {
         producer.setNamesrvAddr("127.0.0.1:9876");
         producer.start();
 
-        producer.setRetryTimesWhenSendAsyncFailed(1000);//设置异步发送失败的超时重试时间
-
         //设置消息主题和消息标签
         String topic = "topic-one";
         String tag = "tag-one";
@@ -142,6 +140,109 @@ class MqcodeApplicationTests {
         //异步发送
         producer.sendOneway(message);
 
+    }
+
+    //=======下边是发送顺序消息==========
+    /**
+     * 顺序消息，按照顺序发送消息，消费者也按照顺序消费消息
+     * @throws Exception
+     */
+    @Test
+    public void testRocketMQProduceAPIOrder() throws Exception {
+        //初始化消息
+        String[] msgs = {
+                "15103111039,创建",
+                "15103111065,创建",
+                "15103111039,付款",
+                "15103117235,创建",
+                "15103111065,付款",
+                "15103117235,付款",
+                "15103111065,完成",
+                "15103111039,推送",
+                "15103117235,完成",
+                "15103111039,完成"
+        };
+
+
+        /**
+         * 创建消息发送者 并指定生产者组
+         */
+        DefaultMQProducer producer = new DefaultMQProducer("producer-one-test");
+        /**
+         * 设置nameserver，讲自己交给其管理
+         */
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+
+
+        //设置消息主题和消息标签
+        String topic = "topic-one";
+        String tag = "tag-one";
+
+        for(String s:msgs){
+            Message message=new Message(topic,tag,s.getBytes());
+
+            String[] split = s.split(",");
+            long orderId=Long.parseLong(split[0]);
+
+            /*
+            MessageQueueSelector用来选择发送的队列,
+            这里用订单的id对队列数量取余来计算队列索引
+
+            send(msg, queueSelector, obj)
+            第三个参
+             */
+
+            SendResult send = producer.send(message, new MessageQueueSelector() {
+                /**
+                 *
+                 * @param list 消息队列里的所有消息
+                 * @param message  消息
+                 * @param o 传入的orderId
+                 * @return
+                 */
+                @Override
+                public MessageQueue select(List<MessageQueue> list, Message message, Object o) {
+                    Long orderId = (Long) o;
+                    //订单id对队列数量取余, 相同订单id得到相同的队列索引
+                    long index = orderId % list.size();
+                    System.out.println("消息已发送到: "+list.get((int) index));
+                    return list.get((int) index);
+                }
+            }, orderId);
+
+            System.out.println(send);
+        }
+
+    }
+
+    @Test
+    public void testRocketMQConsumerAPIOrder() throws Exception {
+        /**
+         * 创建消息消费者，有pull和push两种方式，pull需要消费者自己去拉取消息，push方式Broker会主动讲消息推送给消费者
+         *
+         * 设置消费者组，同一组消费者 必须消费相同的消息
+         */
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer-test-one");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+
+        //订阅消息 主题  标签
+        consumer.subscribe("topic-one", "tag-one");
+        //监听消息
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> list, ConsumeOrderlyContext consumeOrderlyContext) {
+                //获取当前线程名字
+                String name = Thread.currentThread().getName();
+
+                for (MessageExt msg : list) {
+                    System.out.println(name+" - "+ msg.getQueueId() + " - " +new String(msg.getBody()));
+                }
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+
+        consumer.start();
     }
 
 
