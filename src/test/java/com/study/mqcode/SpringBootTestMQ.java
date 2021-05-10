26d9984ed2316b02bfb5d64e8e3cd09ae61aaaae
LYN_CODE <1304271496@qq.com>
2021-05-10 14:51:54 +0800

    发送同步  异步 顺序消息

--- src/test/java/com/study/mqcode/SpringBootTestMQ.java
+++ src/test/java/com/study/mqcode/SpringBootTestMQ.java
@@ -1,5 +1,7 @@
 package com.study.mqcode;
 
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +25,35 @@ public class SpringBootTestMQ {
 
     @Test
     public void testBootMQOne(){
+        //发送同步消息
         rocketMQTemplate.send("ss", MessageBuilder.withPayload("一条具体的消息").build());
+        rocketMQTemplate.send(MessageBuilder.withPayload("一条具体的消息").build());
+
+        //发送异步消息
+        rocketMQTemplate.asyncSend("topicone","一条可爱的消息",new SendCallback(){
+
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.println("成功了");
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                System.out.println("失败了");
+            }
+        });
+
+        //发送一个顺序消息
+        rocketMQTemplate.asyncSendOrderly("topicone", "一条正经的消息", "00", new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.println("成功了");
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                System.out.println("失败了");
+            }
+        });
     }
 }
